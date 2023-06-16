7258cd7b6fa8a137213e753b5d4099a08f301206
yasitha <noothigattuyasitha@gmail.com>
2023-06-17 02:42:40 +0530

    title changed

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 13, 45, 100)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Home page'),
+      home: const MyHomePage(title: 'App Home page'),
     );
   }
 }
